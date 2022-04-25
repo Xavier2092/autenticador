ee35f2baddb72dcc15a911a31fc3d22324113257
Xavier <2092.rxcm@gmail.com>
2022-04-25 13:57:59 -0500

    autenticador2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+
+
+  const responseGoogle = (respuesta)=> {
+    console.log(respuesta);
+    alert(`Bienvenido ${respuesta.profileObj.givenName}`)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <br/><br/>
+    <br/><br/>
+      <GoogleLogin
+        clientId="1019009893763-dflj9lhtopknvehpgt0e20tvdga25q0m.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
